8cf7d52795bbd2b2100e555540a1fa9d921c1437
Leo Jiménez <leojimenezcr@gmail.com>
2017-10-29 14:02:25 -0600

    Estructura del archivo

---
+++ lisp/961611-B24882-A34045-Tarea_programada_II.lisp
@@ -0,0 +1,53 @@
+;; TAREA PROGRAMADA II
+;;
+;; Escuela​ ​ de​ ​ Ciencias​ ​ de​ ​ la​ ​ Computación​ ​ e Informática
+;; CI-1441​ ​ - ​ ​ Paradigmas​ ​ Computacionales
+;; Prof.​ ​ Alvaro​ ​ de​ ​ la​ ​ Ossa​ ​ O.
+;;
+;; Estudiantes:
+;; Leonardo​ ​ Jiménez​ ​ Quijano,​ ​ 961611
+;; Daniel​ ​ Orozco​ ​ Venegas,​ ​ B24882
+;; Fanny​ ​ Porras​ ​ Zúñiga,​ ​ A34045
+
+
+
+;; Procesamiento de arboles y conjuntos ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Busqueda por profundidad primero ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: documentacion estandar
+(defun bpp (N A))
+
+
+;; Busqueda anchura primero ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: documentacion estandar
+(defun bap (N A))
+
+
+;; Potencia ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: documentacion estandar
+(defun potencia (C))
+
+
+;; Producto cartesiano ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: documentacion estandar
+(defun cartesiano (A B))
+
+
+
+;; La maquina encriptadora ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Encripta ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: documentacion estandar
+(defun encripta (H Ae As))
+
+;; Decripta ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: documentacion estandar
+(defun decripta (H Ae As Ef))
