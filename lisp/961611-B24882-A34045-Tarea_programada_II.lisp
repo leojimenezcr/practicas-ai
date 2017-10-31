fb4b180f6299c152a74891ee37a5024fe902d736
Leo Jiménez <leojimenezcr@gmail.com>
2017-10-30 23:38:32 -0600

    Encripta y decripta con documentación interna

--- lisp/961611-B24882-A34045-Tarea_programada_II.lisp
+++ lisp/961611-B24882-A34045-Tarea_programada_II.lisp
@@ -78,13 +78,18 @@
 
 ;; Encripta ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TODO: documentacion estandar
+;; (encripta H Ae As) -> el resultado de encriptar la hilera de entrada H con un
+;;   engranaje formado por los alfabetos de entrada (Ae) y salida (As), y el
+;;   estado final de la máquina, formado por el par (ae . as), donde ae y as son
+;;   los simbolos de los alfabetos de entrada y salida, respectivamente, en los
+;;   que quedo la máquina luego de encriptar H
+;;
 ;; Ejemplo: (encripta '(h i e c a) '(a b c d e f g h i j) '(0 1 2 3 4 5 6 7 8 9))
 ;;            -> ((7 8 4 2 0) . (a . 0))
 (defun encripta (H Ae As)
   (cond
     ; cuando llega al fin de la busqueda
-    ( (null H) (cons (last Ae) (last As)) )
+    ( (null H) (cons (last Ae)(last As)) )
 
     ; cuando el simbolo coincide con la cabeza del alfabeto de entrada
     ( (equal (car H)(car Ae)) (cons (car As) (encripta (cdr H)(rota Ae)(rota As))) )
@@ -96,9 +101,54 @@
 
 ;; Decripta ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TODO: documentacion estandar
-(defun decripta (H Ae As Ef))
+;; (decripta H Ae As Ef) -> el resultado de decodificar la hilera encriptada H
+;;   usando los alfabetos Ae y As, iniciando en la posición del engranaje
+;;   descrita por el estado final de la máquina (Ef) cuando se encriptó la
+;;   hilera que produjo la hilera H
+;;
+;; Ejemplo:
+;;  (decripta '(7 8 4 2 0) '(a b c d e f g h i j) '(0 1 2 3 4 5 6 7 8 9) '(a 0))
+;;         -> (h i e c a)
+(defun decripta (H Ae As Ef)
+  ; el resultado viene invertido asi que lo reversa para mostrarlo
+  (reversa
+    ; invierte las hileras para decriptar de atras para adelante
+    (decripta*
+      (reversa H)
+      (configura (reversa As) (reversa Ef)) ; rota los alfabetos hasta coincidir
+      (configura (reversa Ae) Ef)           ; con estado final de encriptacion
+    )
+  )
+)
 
-;; Rota
+;; (decripta* HR AeR AsR) -> el resultado de decofificar la hilera de entrada H
+;;   previamente invertida, con un engranaje formado por los alfabetos de
+;;   entrada (Ae) y salida (As), que tambien fueron invertidos.
+(defun decripta* (HR AeR AsR)
+  (cond
+    ; cuando llega al fin de la busqueda
+    ( (null HR) nil )
+
+    ; cuando el simbolo coincide con la cabeza del alfabeto de entrada
+    ( (equal (car HR)(car AeR)) (cons (car AsR) (decripta* (cdr HR)(rota AeR)(rota AsR))) )
+
+    ; cuando no hay coincidencia del simbolo con la cabeza del alfabeto
+    ( t (decripta* HR (rota AeR) (rota AsR)) )
+  )
+)
+
+;; (rota lista) -> El resultado de rotar lista, simulando una lista circular
 (defun rota (lista)
-      (append (cdr lista) (cons (car lista) nil)))
+  (append (cdr lista) (cons (car lista) nil)))
+
+;; (reversa lista) -> El resultado de invertir los valores de la hilera lista
+(defun reversa (lista)
+  (cond ((null lista) '())
+        (t (append (reversa (cdr lista))(list (car lista)))) ) )
+
+;; (configura Ae Ef) -> El resultado de rotar la hilera (Ae) hasta coincidir con
+;;   estado final (Ef)
+(defun configura (Ae Ef)
+  (cond ((equal (car Ae)(car Ef)) Ae)
+        (t (configura (rota Ae) Ef)) )
+)
